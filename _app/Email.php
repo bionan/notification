777a7f8ae7c875e80db4125dc99e42e32d8a4f86
Antonio Sousa <antonio.peres.sousa@agesc-arouca.pt>
2021-12-25 12:31:41 +0000

    Desenvolvendo aplicação

--- _app/Email.php
+++ _app/Email.php
@@ -2,16 +2,51 @@
 
 namespace Notification;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-class Email {
+class Email 
+{
+    private $mail;
 
     public function __construct()
     {   
-        $email = new PHPMailer;
+        $this->mail = new PHPMailer(true);
+
+        try 
+        {
+            //Server settings
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $this->mail->isSMTP();                                            //Send using SMTP
+            $this->mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $this->mail->Username   = 'user@example.com';                     //SMTP username
+            $this->mail->Password   = 'secret';                               //SMTP password
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $this->mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $this->mail->CharSet = "UTF-8";
+            $this->mail->setLanguage("pt");
+            $this->mail->isHTML();
+            $this->mail->setFrom("lixo@lixo.com", "Um Lixo");
+        }
+        catch (Exception $e)
+        {
+            echo "Falha na criação das definições";
+        }
     }
 
-    public function sendMail() 
+    public function sendMail($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName) 
     {
-        echo "Email enviado!";
+        $this->mail->Subject = $subject;
+        $this->mail->Body    = $body;
+        $this->mail->addReplyTo($replyEmail, $replyName);
+        $this->mail->addAddress($addressEmail, $addressName);
+
+        try{
+            $this->mail->send();
+        }
+        catch (Exception $e)
+        {
+            echo "Erro ao enviar email: {$this->mail->ErrorInfo} {$e->getMessage()}";
+        }
     }
 }
--- index.php
+++ index.php
@@ -5,4 +5,12 @@ require __DIR__ . '/bib-ext/autoload.php';
 use Notification\Email;
 
 $novoEmail = new Email;
-$novoEmail->sendMail();
+//var_dump($novoEmail);
+try
+{
+$novoEmail->sendMail("Assunto de teste", "Corpo da mensagem", "reply@gmail.com", "Nome de replay", "enviarpara@gmail.com", "Enviar nome");
+}
+catch (Exception $e)
+{
+    echo "Falha no envio do email: {$e->getMessage()}";
+}
