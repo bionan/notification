efdb0439fee4c3778dec944487806af6bc70aa3c
Antonio Sousa <antonio.peres.sousa@agesc-arouca.pt>
2021-12-26 10:24:29 +0000

    classe de envio de email

--- test/index.php
+++ test/index.php
@@ -1,11 +1,11 @@
 <?php
-require __DIR__ . '/bib-ext/autoload.php';
+require __DIR__ . '/../bib-ext/autoload.php';
 
 
 use Notification\Email;
 
 $novoEmail = new Email;
-//var_dump($novoEmail);
+
 try
 {
 $novoEmail->sendMail("Assunto de teste", "Corpo da mensagem", "reply@gmail.com", "Nome de replay", "enviarpara@gmail.com", "Enviar nome");
